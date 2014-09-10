b26578f6aac2a4944dbfbf5f661148deb3bdb680
Kenneth De Leon <kenneth.deleon@objectpartners.com>
2014-09-09 23:02:48 -0500

    no message

---
+++ src/main/java/com/blacklinuxdude/cabin/Application.java
@@ -0,0 +1,25 @@
+package com.blacklinuxdude.cabin;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    private static final Log logger = LogFactory.getLog(Application.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+    }
+
+
+
+
+}
